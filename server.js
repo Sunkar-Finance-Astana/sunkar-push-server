4f596986802b66ed1ec0666495fb709cf5aa0795
Sunkar-Finance-Astana <sunkarfinance@yandex.kz>
2026-04-19 23:09:36 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
